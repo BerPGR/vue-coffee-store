5fb1bab5f9bb4a5f46c640d480d9d3911f33a232
Bernardo Matuchewski <bernardoPGR01@gmail.com>
2023-09-19 16:54:27 -0300

    Added Hero section and initial info

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -16,6 +16,27 @@
         </div>
       </nav>
     </header>
+
+    <div class="hero-carousel-container">
+      <img :src="require('@/assets/background1.png')"/>
+    </div> <!--This first part of the Hero won't have the carousel-->
+
+    <div class="initial-info">
+      <div class="initial-info-content">
+        <img :src="require('@/assets/startup.png')" alt="">
+        <p>Envio rápido e frete grátis <br>para todo o Brasil!</p>
+      </div>
+
+      <div class="initial-info-content">
+        <img :src="require('@/assets/discount.png')" alt="">
+        <p>Ofertas todos os dias para <br>esquentar seu coração!</p>
+      </div>
+
+      <div class="initial-info-content">
+        <img :src="require('@/assets/credit-card.png')" alt="">
+        <p>10% de desconto com pagamento <br>à vista!</p>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -76,6 +97,36 @@ export default {
   .right-navbar p {
     margin-right: 30px;
     color: #FFF5E1;
+  }
+
+  .hero-carousel-container img {
+    width: 100%;
+  }
+
+  .initial-info {
+    margin-top: 60px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-evenly;
+  }
+
+  .initial-info-content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .initial-info-content img {
+    object-fit: contain;
+    width: 60px;
+  }
+  .initial-info-content p {
+    padding-top: 20px;
+    font-size: 20px;
+    font-family: 'Montserrat', sans-serif;
+    font-weight: 600;
+    text-align: center;
   }
 
 </style>
